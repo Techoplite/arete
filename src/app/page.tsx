0e10cc1b2ebfa8b7d7b9dc41d3e0b2abb2470d57
Techoplite <oriccimirko@gmail.com>
2024-11-23 12:16:26 +0000

    Add and style calendar control bar (no functionality)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Calendar from "@/components/Calendar";
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen px-2 py-16">
       <Calendar />
     </div>
   );
---
+++ src/utils/getCurrentMonth.ts
@@ -0,0 +1,19 @@
+export default function getCurrentMonth(): string {
+  const date = new Date();
+  const monthNames: string[] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return monthNames[date.getMonth()];
+}
+
