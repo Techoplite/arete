0b0924cbc66dfa52ff52e1c83793ebc1291b0cb9
Techoplite <oriccimirko@gmail.com>
2024-11-23 09:58:43 +0000

    Add y margin to calendar day

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -17,7 +17,7 @@ export default function Calendar() {
   return (
     <div className="grid grid-cols-7">
       {days.map((day) => (
-        <div key={day.number} className='flex flex-col'>
+        <div key={day.number} className='flex flex-col my-2'>
           <div className='self-center'>{day.number}</div>
           <div className='rounded-full h-2 w-2 bg-red-500 self-center mt-2.5'/>
           <div className='rounded-full h-2 w-2 bg-green-500 self-center mt-2.5'/>
