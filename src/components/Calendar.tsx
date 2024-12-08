e6fd3d414778d16905306b9c5b30b471a3992d99
Techoplite <oriccimirko@gmail.com>
2024-12-08 17:39:54 +0000

    Remove typo in component HTML

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,17 +1,38 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
 import getDaysInCurrentMonth from "@/utils/getDaysInCurrentMonth";
 import getDaysOfTheWeekNames from "@/utils/getDaysOfTheWeekNames";
 import { DayInMonth } from "@/app/interfaces/DayInMonth";
 import CalendarDaysNames from "./CalendarDaysNames";
 import CalendarDays from "./CalendarDays";
-import CalendarControls from "./CalendarControls";
+import { PREV, NEXT } from "@/app/constants";
+import getCurrentMonth from "@/utils/getCurrentMonth";
+import getCurrentYear from "@/utils/getCurrentYear";
+import getPreviousCurrentNextMonth from "@/utils/getPreviousCurrentNextMonth";
+import ButtonChevron from "./ButtonChevron";
 
 export default function Calendar() {
-  const [days, setDays] = useState<DayInMonth[]>([]);
+  const currentMonth = getCurrentMonth();
+  const currentYear = getCurrentYear();
+  const date = `${currentMonth} ${currentYear}`;
 
+  const [months, setMonths] = useState<string[]>(
+    getPreviousCurrentNextMonth(date)
+  );
+  const [days, setDays] = useState<DayInMonth[]>([]);
   const [daysNames, setDaysNames] = useState<string[]>([]);
+  
+  const middlePosition = months[1];
+  const leftPosition = months[0];
+  const rightPosition = months[2];
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+    const newLocal = (event.target as HTMLButtonElement).id === PREV;
+    if (newLocal) setMonths(getPreviousCurrentNextMonth(leftPosition));
+    if ((event.target as HTMLButtonElement).id === NEXT)
+      setMonths(getPreviousCurrentNextMonth(rightPosition));
+  };
 
   useEffect(() => {
     const daysInMonth = getDaysInCurrentMonth();
@@ -22,7 +43,11 @@ export default function Calendar() {
 
   return (
     <>
-      <CalendarControls />
+      <div className="flex flex-row justify-center align-center">
+        <ButtonChevron position={PREV} handleClick={handleClick} />
+        <div className="w-full text-2xl text-center">{middlePosition}</div>
+        <ButtonChevron position={NEXT} handleClick={handleClick} />
+      </div>
       <CalendarDaysNames daysNames={daysNames} />
       <CalendarDays daysNames={daysNames} days={days} />
     </>
