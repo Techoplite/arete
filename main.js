ed6c3454cf20f7610feed1ede184c7798f84e2c1
Techoplite <oriccimirko@gmail.com>
2025-01-26 18:05:03 +0000

    Display sum of calories from Supabase on page load

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const supabaseUrl = SUPABASE_URL;
 const supabaseKey = SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
-const { data, error } = await supabase.from("calories_log").select();
+const { data, error } = await supabase.from("calories_log").select("calories");
 console.log("data :>> ", data);
 console.log("error :>> ", error);
+
+const totalCaloriesSpan = document.getElementById("total-calories");
+const calories = data?.map((c) => c.calories).reduce((a, b) => a + b, 0);
+totalCaloriesSpan.textContent = calories;
