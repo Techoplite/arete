792d5dc7ad0b5950a13371dd29c9594915a91b96
Techoplite <oriccimirko@gmail.com>
2025-01-26 17:17:56 +0000

    Successfully fetch data from supabase

--- main.js
+++ main.js
@@ -1,47 +1,62 @@
-async function handleCaloriesInput(adverb) {
-  const input = document.getElementById(`calories-${adverb}`);
-  console.log("input :>> ", input);
-  const calories = input?.value;
+// async function handleCaloriesInput(adverb) {
+//   const input = document.getElementById(`calories-${adverb}`);
+//   console.log("input :>> ", input);
+//   const calories = input?.value;
 
-  console.log("calories :>> ", calories);
+//   console.log("calories :>> ", calories);
 
-  const response = await fetch("http://localhost:3000/add-calories-entry", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ calories, operator: adverb === "in" ? "+" : "-" }),
-  });
+//   const response = await fetch("http://localhost:3000/add-calories-entry", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ calories, operator: adverb === "in" ? "+" : "-" }),
+//   });
 
-  if (response.ok) {
-    const data = await response.json();
+//   if (response.ok) {
+//     const data = await response.json();
 
-    // Update the DOM with the total calories
-    document.getElementById("total-calories").textContent = data.totalCalories;
-    input.value = "";
-  }
-}
+//     // Update the DOM with the total calories
+//     document.getElementById("total-calories").textContent = data.totalCalories;
+//     input.value = "";
+//   }
+// }
 
-async function getTotalCalories() {
-  const response = await fetch("http://localhost:3000/get-total-calories");
+// async function getTotalCalories() {
+//   const response = await fetch("http://localhost:3000/get-total-calories");
 
-  if (response.ok) {
-    const data = await response.json();
+//   if (response.ok) {
+//     const data = await response.json();
 
-    document.getElementById("total-calories").textContent = data.totalCalories;
-  }
-}
+//     document.getElementById("total-calories").textContent = data.totalCalories;
+//   }
+// }
 
-getTotalCalories();
+// getTotalCalories();
 
-// Disable submit button if input is empty on page load
-document.querySelectorAll("input[type='text']").forEach((input) => {
-  const submitButton = input.nextElementSibling;
-  submitButton.disabled = !input.value;
-});
+// // Disable submit button if input is empty on page load
+// document.querySelectorAll("input[type='text']").forEach((input) => {
+//   const submitButton = input.nextElementSibling;
+//   submitButton.disabled = !input.value;
+// });
 
-// Enable submit button if input is not empty
-document.querySelectorAll("input[type='text']").forEach((input) => {
-  input.addEventListener("input", () => {
-    const submitButton = input.nextElementSibling;
-    submitButton.disabled = !input.value;
-  });
-});
+// // Enable submit button if input is not empty
+// document.querySelectorAll("input[type='text']").forEach((input) => {
+//   input.addEventListener("input", () => {
+//     const submitButton = input.nextElementSibling;
+//     submitButton.disabled = !input.value;
+//   });
+// });
+
+// // connection string: postgresql://postgres:[YOUR-PASSWORD]@db.gnoijvmiulhfdgptzwit.supabase.co:5432/postgres
+
+//
+//
+//
+
+// import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'";
+// const supabaseUrl = "https://gnoijvmiulhfdgptzwit.supabase.co";
+// const supabaseKey = process.env.SUPABASE_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// const { data, error } = await supabase.from("calories_log").select();
+// console.log("error :>> ", error);
+// console.log("data :>> ", data);
