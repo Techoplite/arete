60b1bb9025f7d64c48064bd6a36bbe525da49e7e
Techoplite <oriccimirko@gmail.com>
2025-01-26 17:31:41 +0000

    Refactor to only use main.js and local variables

--- server.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://gnoijvmiulhfdgptzwit.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-const { data, error } = await supabase.from("calories_log").select();
-console.log("error :>> ", error);
-console.log("data :>> ", data);
