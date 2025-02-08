32194542bc08f1b1a43eb08680abe74c78f69c38
Techoplite <oriccimirko@gmail.com>
2025-02-08 19:26:42 +0000

    Display previous days list

--- main.js
+++ main.js
@@ -6,19 +6,43 @@ const supabaseKey = SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+function groupCaloriesByDay(entries) {
+  return entries.reduce((acc, entry) => {
+    // Format date as D/M/YYYY
+    let date = new Date(entry.created_at).toLocaleDateString("en-GB");
+
+    // Aggregate calories
+    acc[date] = (acc[date] || 0) + entry.calories;
+
+    return acc;
+  }, {});
+}
+
 const getCaloriesValue = async () => {
   const { data } = await supabase.from("calories_log").select();
 
-  const totalCaloriesSpan = document.getElementById("total-calories");
-  const calories = data?.map((c) => c.calories).reduce((a, b) => a + b, 0);
-  totalCaloriesSpan.textContent = calories;
+  const caloriesGroupedByDay = groupCaloriesByDay(data);
 
-  latestId = data[data.length - 1].id;
+  const today = new Date().toLocaleDateString("en-GB");
+  const caloriesToday = groupCaloriesByDay(data)[today] || 0;
+
+  const totalCaloriesSpanEl = document.getElementById("total-calories");
+
+  totalCaloriesSpanEl.textContent = caloriesToday;
+
+  const previousDaysUlEl = document.getElementById("previous-days");
+
+  Object.keys(caloriesGroupedByDay)
+    .slice(0, -1)
+    .reverse()
+    .map((day) => {
+      const li = document.createElement("li");
+      li.textContent = `${day}: ${caloriesGroupedByDay[day]}`;
+      previousDaysUlEl.appendChild(li);
+    });
 };
 
 getCaloriesValue();
-
-let latestId;
 
 const handleCaloriesInput = async (adverb) => {
   const caloriesInput = document.getElementById(`calories-${adverb}`).value;
