710c5a9bc1f241ef40d1d04fce8ff2aa9f12339e
Techoplite <oriccimirko@gmail.com>
2025-10-04 12:33:39 +0100

    Make PWA

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  fallbacks: {
+    document: "/~offline",
+  },
+});
+
+export default withPWA({
+  // Your Next.js config
+});
