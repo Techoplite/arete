81dffbcbfb1f40731f4f351bac2782b944b37a6b
Techoplite <oriccimirko@gmail.com>
2025-10-04 11:48:37 +0100

    Remove height on wellness buttons

--- src/components/WellnessButtons.tsx
+++ src/components/WellnessButtons.tsx
@@ -2,7 +2,7 @@ import WellnessButton from "./WellnessButton";
 
 export default function WellnessButtons() {
   return (
-    <div className="flex gap-2.5 flex-col w-full h-[-webkit-fill-available] flex-1">
+    <div className="flex gap-2.5 flex-col w-full flex-1">
       <WellnessButton label="Training" color="red" />
       <WellnessButton label="Nutrition" color="green" />
     </div>
