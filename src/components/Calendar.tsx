36781449b9e61c9b058bd40fb8528aa4fb087afb
Techoplite <oriccimirko@gmail.com>
2025-10-04 10:37:05 +0100

    Reduce text size

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -46,7 +46,7 @@ export default function Calendar() {
     <>
       <div className="flex flex-row justify-center align-center">
         <ButtonChevron position={PREV} handleClick={handleClick} />
-        <div className="w-full text-2xl text-center self-center">
+        <div className="w-full text-l text-center self-center">
           {middlePosition}
         </div>
         <ButtonChevron position={NEXT} handleClick={handleClick} />
--- src/components/CalendarDay.tsx
+++ src/components/CalendarDay.tsx
@@ -27,7 +27,7 @@ export default function CalendarDay({
       key={day.number}
       className={`flex flex-col py-2 ${isToday && "bg-dark"}`}
     >
-      <div className="self-center text-xl">{day.number}</div>
+      <div className="self-center">{day.number}</div>
       <div className="rounded-full h-2 w-2 bg-red-500 self-center my-2.5" />
       <div className="rounded-full h-2 w-2 bg-green-500 self-center" />
     </li>
