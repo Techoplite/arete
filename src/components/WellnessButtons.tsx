371d28a1b285139a2692e76aba4c900b89a994d0
Techoplite <oriccimirko@gmail.com>
2025-10-04 10:54:34 +0100

    Wellness buttons on a row

--- src/components/WellnessButtons.tsx
+++ src/components/WellnessButtons.tsx
@@ -2,7 +2,7 @@ import WellnessButton from "./WellnessButton";
 
 export default function WellnessButtons() {
   return (
-    <div className="flex gap-2.5 flex-col w-full h-[-webkit-fill-available] flex-1 mt-6">
+    <div className="flex gap-2.5 flex-row w-full h-[-webkit-fill-available] flex-1 mt-6">
       <WellnessButton label="Training" color="red" />
       <WellnessButton label="Nutrition" color="green" />
     </div>
